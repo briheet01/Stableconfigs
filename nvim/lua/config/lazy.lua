3dbf86fa7760920c278e8341e892155f1078682e
briheet01 <briheetyadav@gmail.com>
2024-03-15 17:04:27 +0530

    wall & nvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,6 +23,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
     { import = "plugins" },
+
+    -- Add presence.nvim as a lazy-loaded plugin
+    { "andweeb/presence.nvim", disable = false },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
