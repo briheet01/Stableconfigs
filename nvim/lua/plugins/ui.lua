6c8c6a98178e39a0a7eef67c3f5da00e64e7d356
briheet01 <briheetyadav@gmail.com>
2024-04-20 10:18:40 +0530

    changes

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -75,11 +75,11 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VeryLazy",
-    -- opts = {
-    -- options = {
-    -- theme = "purple",
-    -- },
-    -- },
+    opts = {
+      options = {
+        thme = "purple",
+      },
+    },
   },
 
   {
