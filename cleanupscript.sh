4bf07124eda0a063ab277872476c3f7a38eaa9bd
Briheet Singh Yadav <briheetyadav@gmail.com>
2024-09-14 00:17:26 +0530

    hhhhggghhhh

--- cleanupscript.sh
+++ cleanupscript.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-rm -rf /home/briheet/*.png
-rm -rf ~/.local/state/nvim/swap/*.swp
+shopt -s nullglob
+
+# Move PNG files if they exist
+if compgen -G "*.png" >/dev/null; then
+  mv ./*.png /home/briheet/Pictures/Screenshots/
+fi
+
+# Remove swap files if they exist
+if ls ~/.local/state/nvim/swap/*.swp 1>/dev/null 2>&1; then
+  rm -rf ~/.local/state/nvim/swap/*.swp
+fi
