7ee8e919ac880bdcd5978c7b075af628e7daa3de
briheet <briheetyadav@gmail.com>
2025-01-29 16:00:15 +0530

    nvim: solarized

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,9 +14,9 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       -- ,
-      --   opts = {
-      --   colorscheme = "solarized-osaka",
-      -- }
+      opts = {
+        colorscheme = "solarized-osaka",
+      },
     },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
